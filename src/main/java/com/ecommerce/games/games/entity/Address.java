173765346f4e971c8d6236a36dd7bd7b0eb5de03
Rafael Ribeiro <rafaelflarrn@gmail.com>
2020-11-14 02:01:57 -0300

    Add address entity

---
+++ src/main/java/com/ecommerce/games/games/entity/Address.java
@@ -0,0 +1,88 @@
+package com.ecommerce.games.games.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String zipCode;
+    private String city;
+    private String country;
+    private int number;
+    private String street;
+
+    public Address() {
+    }
+
+    public Address(Long id, String name, String zipCode, String city, String country, int number, String street) {
+        this.id = id;
+        this.name = name;
+        this.zipCode = zipCode;
+        this.city = city;
+        this.country = country;
+        this.number = number;
+        this.street = street;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+}
