2d78c53dcfa299d479ef667a09bdb9fa04b82129
Rafael Ribeiro <rafaelflarrn@gmail.com>
2020-11-16 23:36:02 -0300

    add shipping entity

---
+++ src/main/java/com/ecommerce/games/games/entity/Shipping.java
@@ -0,0 +1,60 @@
+package com.ecommerce.games.games.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Shipping {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date estimatedDelivery;
+    private float price;
+    private Address address;
+
+    public Shipping() {
+    }
+
+    public Shipping(Long id, Date estimatedDelivery,
+                    float price, Address address) {
+        this.id = id;
+        this.estimatedDelivery = estimatedDelivery;
+        this.price = price;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getEstimatedDelivery() {
+        return estimatedDelivery;
+    }
+
+    public void setEstimatedDelivery(Date estimatedDelivery) {
+        this.estimatedDelivery = estimatedDelivery;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
