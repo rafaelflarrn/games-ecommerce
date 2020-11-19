28460ff6a7730e2205b9b8d95cf857879a80a35e
Rafael Ribeiro <rafaelflarrn@gmail.com>
2020-11-19 01:03:14 -0300

    add payment repository

--- src/main/java/com/ecommerce/games/games/repository/CouponRepository.java
+++ src/main/java/com/ecommerce/games/games/repository/CouponRepository.java
@@ -1,5 +1,6 @@
-package com.ecommerce.games.games.entity;
+package com.ecommerce.games.games.repository;
 
+import com.ecommerce.games.games.entity.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
